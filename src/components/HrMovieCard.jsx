de03d3a473489e77842b24eb66543c37f924a885
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-12-23 00:26:33 +0530

    added

--- src/components/HrMovieCard.jsx
+++ src/components/HrMovieCard.jsx
@@ -6,7 +6,7 @@ const HrMovieCard = ({ movie }) => {
   return (
     <section className='hover:scale-110 transition-all duration-150 ease-in cursor-pointer'>
     <img src={IMAGE_BASE_URL + movie.backdrop_path} alt="" className="w-[110px] md:w-[260px] rounded-lg hover:border-[4px] text-gray-400 hover:scale-110 transition-all duration-150 ease-in cursor-pointer"/>
-    <h2 className="w-[110px] md:w-[260px] text-white font-bold mt-3">{movie.title}</h2>
+    <h2 className="w-[100px] md:w-[260px] text-white font-bold mt-3">{movie.title}</h2>
   </section>
   )
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -28,8 +28,8 @@ const Slider = () => {
 
   return (
     <div>
-      <IoIosArrowBack className='hidden md:block absolute text-white text-[30px] mx-8 mt-[150px] cursor-pointer' onClick={()=>SliderLeft(elementRef.current)}/>
-      <IoIosArrowForward className='hidden md:block absolute text-white text-[30px] mx-8 mt-[150px] cursor-pointer right-0' onClick={()=>sliderRight(elementRef.current)}/>
+      <IoIosArrowBack className='hidden md:block absolute text-white text-[30px] mx-8 mt-[200px] cursor-pointer' onClick={()=>SliderLeft(elementRef.current)}/>
+      <IoIosArrowForward className='hidden md:block absolute text-white text-[30px] mx-8 mt-[200px] cursor-pointer right-0' onClick={()=>sliderRight(elementRef.current)}/>
     <div className='flex overflow-x-auto px-16 py-4 w-full scrollbar-none scroll-smooth' ref={elementRef}> 
       {movieList.map((item,index) => (
         <img src={IMAGE_BASE_URL + item.backdrop_path} alt="" className='min-w-full md:h-[420px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in' key={index}/>
